40563ad786038d96e5695405c839e353a1567f07
maru0pengin <maru0pengin@gmail.com>
2021-06-26 13:37:16 +0900

    最初のコミット

---
+++ src/views/Home.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <v-row >
+      <v-col>
+        <v-card class="mx-auto" width="300" height="330">
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            :src=articles[0].image.url
+          >
+            <v-card-title>Top 10 Australian beaches</v-card-title>
+          </v-img>
+
+          <v-card-text class="text--primary">
+            <div>Whitehaven Beach</div>
+
+            <div>Whitsunday Island, Whitsunday Islands</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="orange" text>More</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+
+      <v-col>
+        <v-card class="mx-auto" width="300" height="330">
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          >
+            <v-card-title>Top 10 Australian beaches</v-card-title>
+          </v-img>
+
+          <v-card-text class="text--primary">
+            <div>Whitehaven Beach</div>
+
+            <div>Whitsunday Island, Whitsunday Islands</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="orange" text>More</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+
+      <v-col>
+        <v-card class="mx-auto" width="300" height="330">
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          >
+            <v-card-title>Top 10 Australian beaches</v-card-title>
+          </v-img>
+
+          <v-card-text class="text--primary">
+            <div>Whitehaven Beach</div>
+
+            <div>Whitsunday Island, Whitsunday Islands</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="orange" text>More</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Home',
+
+  data: () => ({
+    articles: null,
+  }),
+
+  async mounted() {
+    // 記事を取得する
+    console.log(process.env)
+    console.log(`${process.env.VUE_APP_X_API_KEY}`)
+    const response = await axios
+      .get('https://maru0pengin.microcms.io/api/v1/products', {
+        headers: { 'X-API-KEY': process.env.VUE_APP_X_API_KEY },
+      })
+      .catch((e) => console.log(e))
+    this.articles = response.data.contents
+    console.log(this.articles)
+  },
+}
+</script>
+<style scoped>
+.summary {
+  white-space: pre-wrap;
+}
+</style>
